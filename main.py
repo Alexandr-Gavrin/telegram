ac95eb808b98d2bb6ca8dca87df9267f4d8c521d
Александр <nuke.dust@yandex.ru>
2020-03-23 11:23:44 +0300

    2 Commit

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 
-
 TOKEN = '620473623:AAHGmWT2-NRSivii37elUlp1uKe5WNYGauM'
-
 
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
+
+
+def start(update, context):
+    update.message.reply_text('Привет! Я - эхо-бот!. Напиши мне что нибудь!')
+
+
+def help(update, context):
+    update.message.reply_text('Я пока не умею помогать!((')
 
 
 def main():
@@ -15,6 +21,8 @@ def main():
     text_handler = MessageHandler(Filters.text, echo)
     dp = updater.dispatcher
     dp.add_handler(text_handler)
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
     updater.start_polling()
     updater.idle()
 
