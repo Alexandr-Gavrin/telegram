0200ee18745c1f04d8b75a4d8cc0b37ebc41f3c1
Александр <nuke.dust@yandex.ru>
2020-05-10 13:13:07 +0300

    date-time4

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import time
 
 
-def date(update, context):
+def dates(update, context):
     t = time.localtime()
     data = time.asctime(t).split()
     update.message.reply_text(f'Date - {data[0]} {data[1]} {data[2]} {data[4]}')
@@ -19,7 +19,7 @@ def main():
     updater = Updater('1187007411:AAE61PeWLmJRb9BD9U2L9dIV-igj0fq3Uxs', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("time", times))
-    dp.add_handler(CommandHandler("date", date))
+    dp.add_handler(CommandHandler("date", dates))
     updater.start_polling()
     updater.idle()
 
