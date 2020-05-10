b4d79a7d615f6f395a4282917ced75769f4ef1d7
Александр <nuke.dust@yandex.ru>
2020-05-10 13:33:07 +0300

    3

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-import time
 
 
-def dates(update, context):
-    t = time.localtime()
-    data = time.asctime(t).split()
-    update.message.reply_text(f'Date - {data[0]} {data[1]} {data[2]} {data[4]}')
+def task(context):
+    job = context.job
+    context.bot.send_message(job.context, text='Вернулся!')
 
 
-def times(update, context):
-    t = time.localtime()
-    data = time.asctime(t).split()
-    update.message.reply_text(f'Time - {data[3]}')
+def unset_timer(update, context):
+    # Проверяем, что задача ставилась
+    if 'job' not in context.chat_data:
+        update.message.reply_text('Нет активного таймера')
+        return
+    job = context.chat_data['job']
+    # планируем удаление задачи (выполнится, когда будет возможность)
+    job.schedule_removal()
+    # и очищаем пользовательские данные
+    del context.chat_data['job']
+    update.message.reply_text('Хорошо, вернулся сейчас!')
+
+
+def set_timer(update, context):
+    chat_id = update.message.chat_id
+    try:
+        due = int(context.args[0])
+        if due < 0:
+            update.message.chat_id.reply_text('Время задано некорректно')
+            return
+        if 'job' in context.chat_data:
+            old_job = context.chat_data['job']
+            old_job.schedule_removal()
+        new_job = context.job_queue.run_once(task, due, context=chat_id)
+        context.chat_data['job'] = new_job
+        update.message.reply_text(f'Вернусь через {due} секунд')
+    except (IndexError, ValueError):
+        update.message.reply_text('Использование: /set <секунд>')
 
 
 def main():
     updater = Updater('1187007411:AAE61PeWLmJRb9BD9U2L9dIV-igj0fq3Uxs', use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("time", times))
-    dp.add_handler(CommandHandler("date", dates))
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unset", unset_timer,
+                                  pass_chat_data=True)
+                   )
     updater.start_polling()
     updater.idle()
 
